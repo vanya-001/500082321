5b399799e3621f12cc6c2e2254012271b0f940b5
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-09-10 12:44:42 +0530

    Page 1 Created

--- Q1/frontend/src/components/TrainSchedule.js
+++ Q1/frontend/src/components/TrainSchedule.js
@@ -1,17 +1,17 @@
-import React, {usestate, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const TrainSchedule = () => {
-    const [trains, setTrains] = usestate([]);
+    const [trains, setTrains] = useState([]);
 
     useEffect(() => {
-        axios.get('http://20.244.56.144/train/trains')
+        axios.get('http://20.244.56.144/train/trains/2344')
         .then(response =>{
             setTrains(response.data);
         })
         .catch(error => {
             console.log('Error Found' , error);
-        })
+        });
     }, []);
 
     return(
@@ -23,27 +23,25 @@ const TrainSchedule = () => {
                         <th>Train Name</th>
                         <th>Train Number</th>
                         <th>Departure Time</th>
-                        <th>Seat Available</th>
-                        <th>Sleeper</th>
-                        <th>AC</th>
-                        <th>Price</th>
-                        <th>Sleeper</th>
-                        <th>AC</th>
+                        <th>Sleeper Available</th>
+                        <th>AC Available</th>
+                        <th>Sleeper Price</th>
+                        <th>AC Price</th>
                         <th>Delay By</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {trains.map(train =>(
+                    {trains.map(train => (
                         <tr key ={train.trainNumber}>
-                            <td>{train.trainName}</td>
-                            <td>{train.trainNumber}</td>
-                            <td>{`${train.departureTime.Hours}: ${train.departureTime.Minutes}: ${train.departureTime.Seconds}`}</td>
-                            <td>{train.seatsAvailable.sleeper}</td>
-                            <td>{train.seatsAvailable.AC}</td>
-                            <td>{train.price.sleeper}</td>
-                            <td>{train.price.AC}</td>
-                            <td>{train.delayedBy}</td>
-                        </tr>
+                        <td>{train.trainName}</td>
+                        <td>{train.trainNumber}</td>
+                        <td>{`${train.departureTime.Hours || ''}: ${train.departureTime.Minutes || ''}: ${train.departureTime.Seconds || ''}`}</td>
+                        <td>{train.seatsAvailable.sleeper}</td>
+                        <td>{train.seatsAvailable.AC}</td>
+                        <td>{train.price.sleeper}</td>
+                        <td>{train.price.AC}</td>
+                        <td>{train.delayedBy}</td>
+                </tr>
                     ))}
                 </tbody>
             </table>
